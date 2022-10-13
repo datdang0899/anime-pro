189e4cd877df9288acd6b73620f34c8e652c297c
datdang <105406960+datdang0899@users.noreply.github.com>
2022-10-13 21:12:36 +0700

    deploy anime-pro

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { AnimeProvider } from "../src/context/AnimeContext";
+import Layout from "./layout";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-toastify/dist/ReactToastify.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
